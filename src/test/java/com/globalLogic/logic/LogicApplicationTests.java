0565ca82784fb3d7df7922a41f0a2f59b81b780a
Joanna Rzeszotko-Doniec <rivienne8@gmail.com>
2021-05-19 18:36:33 +0200

    creating index page with basic styles

---
+++ src/test/java/com/globalLogic/logic/LogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalLogic.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
